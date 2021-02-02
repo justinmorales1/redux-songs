e31bd8c6b42756e31d782b0a02fd8fcdf055e188
justinmorales1 <justin@justins-mbp.lan>
2021-02-01 20:24:43 -0600

    Adding components and reducers

---
+++ src/components/SongDetail.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { connect } from "react-redux";
+
+const SongDetail = ({mySelectedSong}) => {
+    if(!mySelectedSong){
+        return (
+            <div>
+                Select A Song
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <h3> Details for: </h3>
+            <p> Title: {mySelectedSong.title}
+            <br/>
+                Duration: {mySelectedSong.duration}
+            </p>
+
+        </div>
+    )
+};
+
+
+const mapStateToProps = (state) => {
+    return {
+        mySelectedSong: state.selectedSong
+    }
+}
+
+export default connect(mapStateToProps)(SongDetail);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./components/App";
-import {Provider} from 'react-redux'
-
+import {Provider} from 'react-redux';
+import  reducers  from './reducers'
+import { createStore } from "redux";
 
 
 
 ReactDOM.render(
-    <Provider>
-        <App/>
+    <Provider store={createStore(reducers)}>
+        <App/>,
     </Provider>,
     document.getElementById("root"));
